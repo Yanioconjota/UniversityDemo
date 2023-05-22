f3f525f6ee01abbbbf7885c0f06f03f480e7145e
Janio Isacura <janioisacura@gmail.com>
2023-05-22 18:38:50 -0300

    Base project added

---
+++ UniversityApiBackend/Models/DataModels/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class User: BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = String.Empty;
+
+        [Required, StringLength(100)]
+        public string LastName { get; set; } = String.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = String.Empty;
+
+        [Required]
+        public string Password { get; set; } = String.Empty;
+    }
+}
