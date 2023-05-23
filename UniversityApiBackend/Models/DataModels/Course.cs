df9194caec485d4b5c2a2aca7db7677b0d43976e
Janio Isacura <janioisacura@gmail.com>
2023-05-23 10:43:40 -0300

    New tabled added

---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public enum CourseLevel
+    {
+        Basic,
+        Intermediate,
+        Advanced
+    }
+
+    public class Course: BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = String.Empty;
+        
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = String.Empty;
+        
+        public string? LongDescription { get; set; } = String.Empty;
+
+        [Required]
+        public string PublicObjective { get; set; } = String.Empty;
+
+        [Required]
+        public string Goals { get; set; } = String.Empty;
+        
+        [Required]
+        public CourseLevel Level { get; set; }
+    }
+}
